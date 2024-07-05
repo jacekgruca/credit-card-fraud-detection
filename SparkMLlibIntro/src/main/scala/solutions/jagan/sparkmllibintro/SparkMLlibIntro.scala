0f27db1fcc80fa7f00e0db4fe7ba61962c6c6b02
Jacek Aleksander Gruca <jacek@gruca.net>
2024-07-05 12:38:48 +0200

    replace the logistic regression method with random forest

--- SparkMLlibIntro/src/main/scala/solutions/jagan/sparkmllibintro/SparkMLlibIntro.scala
+++ SparkMLlibIntro/src/main/scala/solutions/jagan/sparkmllibintro/SparkMLlibIntro.scala
@@ -1,7 +1,8 @@
 package solutions.jagan.sparkmllibintro
 
-import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithLBFGS}
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.tree.model.RandomForestModel
+import org.apache.spark.mllib.tree.RandomForest
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.stat.Statistics
@@ -17,7 +18,8 @@ object SparkMLlibIntro {
   // this is the Kaggle Credit Card Fraud Detection dataset: https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud
   private val inputFilename = "../datasets/credit-card-fraud-detection/creditcard-small.csv"
   private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH.mm.ss.SSS")
-  private val master = "local[100]"
+  private val noOfInputColumns = 30
+  private val master = "local[64]"
   private val numClasses = 2
 
   private val objectName = this.getClass.getSimpleName.stripSuffix("$")
@@ -39,15 +41,16 @@ object SparkMLlibIntro {
     val (trainingData, testData) = splitData(labeledData, seed)
     val (model, metrics) = performTraining(trainingData, testData, numClasses)
     displayMetrics(metrics)
+
     val reloadedModel = reloadModel(model)
     demonstrateReloadedModel(reloadedModel)
 
     sc.stop()
   }
 
-  private def demonstrateReloadedModel(reloadedModel: LogisticRegressionModel) = {
+  private def demonstrateReloadedModel(reloadedModel: RandomForestModel) = {
 
-    val newData: Array[Double] = Array(1.0) ++ Array.fill(28)(1.0) ++ Array(1.0)
+    val newData: Array[Double] = Array(34628.0) ++ Array.fill(noOfInputColumns - 2)(1.0) ++ Array(8.44)
     val newDataAsVector = Vectors.dense(newData)
     val prediction = reloadedModel.predict(newDataAsVector)
 
@@ -55,11 +58,11 @@ object SparkMLlibIntro {
 
   }
 
-  private def reloadModel(model: LogisticRegressionModel) = {
+  private def reloadModel(model: RandomForestModel) = {
 
-    val pathToModel = "model/CCFD_LR_" + LocalDateTime.now.format(formatter)
+    val pathToModel = "model/CCFD_RF_" + LocalDateTime.now.format(formatter)
     model.save(sc, pathToModel)
-    LogisticRegressionModel.load(sc, pathToModel)
+    RandomForestModel.load(sc, pathToModel)
 
   }
 
@@ -71,8 +74,8 @@ object SparkMLlibIntro {
     val rows = data.collect.map {
       case line: String if line.split(",").length > 1 =>
         val splitLine = line.split(",")
-        val inputElements = splitLine.slice(0, 30).map { elem => elem.toDouble }
-        val fraudClass: Int = splitLine(30).replace("\"", "").toInt
+        val inputElements = splitLine.slice(0, noOfInputColumns).map { elem => elem.toDouble }
+        val fraudClass: Int = splitLine(noOfInputColumns).replace("\"", "").toInt
         (Vectors.dense(inputElements), Vectors.dense(inputElements :+ fraudClass.toDouble))
     }
     val rowsNoLabels = rows.map(_._1)
@@ -102,7 +105,7 @@ object SparkMLlibIntro {
 
     sc.parallelize {
       rows.map { row =>
-        LabeledPoint(row(30), Vectors.dense(row.toArray.slice(0, 30)))
+        LabeledPoint(row(noOfInputColumns), Vectors.dense(row.toArray.slice(0, noOfInputColumns)))
       }
     }
 
@@ -137,36 +140,45 @@ object SparkMLlibIntro {
 
   private def displayMetrics(metrics: BinaryClassificationMetrics): Unit = {
 
-    println
-
-    // Precision by threshold
-    val precision = metrics.pr
+    // precision by threshold
+    val precision = metrics.precisionByThreshold
     precision.foreach { case (t, p) =>
-      println(s"Threshold: $t, Precision: $p")
+      println(s"threshold: $t, precision: $p")
     }
 
-    // Recall by threshold
+    // recall by threshold
     val recall = metrics.recallByThreshold
     recall.foreach { case (t, r) =>
-      println(s"Threshold: $t, Recall: $r")
+      println(s"threshold: $t, recall: $r")
     }
 
-    // F-measure
-    val f1Score = metrics.fMeasureByThreshold
-    f1Score.foreach { case (t, f) =>
-      println(s"Threshold: $t, F-score: $f, Beta = 1")
+    // f-measure
+    val fScore = metrics.fMeasureByThreshold
+    fScore.foreach { case (t, f) =>
+      println(s"threshold: $t, f-score: $f, beta = 1")
     }
 
-    // AUPRC
-    val auPRC = metrics.areaUnderPR
-    println("Area under precision-recall curve = " + auPRC)
+    println
+    println(s"area under precision-recall curve (AUPRC) = ${metrics.areaUnderPR}")
+    println(s"area under receiver operating characteristic (AUROC) = ${metrics.areaUnderROC}")
 
   }
 
   private def performTraining(trainingData: RDD[LabeledPoint], testData: RDD[LabeledPoint],
-                              numClasses: Int): (LogisticRegressionModel, BinaryClassificationMetrics) = {
+                              numClasses: Int): (RandomForestModel, BinaryClassificationMetrics) = {
 
-    val model = new LogisticRegressionWithLBFGS().setNumClasses(numClasses).run(trainingData)
+    val numClasses = 2
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val numTrees = 100
+    val featureSubsetStrategy = "auto"
+    val impurity = "gini"
+    val maxDepth = 4
+    val maxBins = 32
+
+    val model: RandomForestModel = RandomForest.trainClassifier(
+      trainingData, numClasses, categoricalFeaturesInfo, numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins
+    )
+
     val predictionAndLabels = testData.map(p => (model.predict(p.features), p.label))
     val metrics = new BinaryClassificationMetrics(predictionAndLabels)
 
@@ -175,7 +187,7 @@ object SparkMLlibIntro {
 
   private def splitData(labeledData: RDD[LabeledPoint], seed: Long): (RDD[LabeledPoint], RDD[LabeledPoint]) = {
 
-    val splits = labeledData.randomSplit(Array(0.8, 0.2), seed)
+    val splits = labeledData.randomSplit(Array(0.7, 0.3), seed)
     val trainingData = splits(0)
     val testData = splits(1)
 
